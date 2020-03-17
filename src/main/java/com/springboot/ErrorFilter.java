9aa6d70012dc3c4369c66a6282f7bbd36ad09519
neelam <das.neelam@unisys.com>
2020-03-17 17:00:26 +0530

    spring-boot-zuulgateway-proxy

---
+++ src/main/java/com/springboot/ErrorFilter.java
@@ -0,0 +1,32 @@
+package com.springboot;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+public class ErrorFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "error";
+	}
+
+	@Override
+	public int filterOrder() {
+		System.out.println("Inside Error Filter");
+		return 1;
+	}
+
+}
